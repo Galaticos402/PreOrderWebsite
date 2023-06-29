0dd9fa9d4dea3c450470bc310bd1e1919b99b3f5
HungNQ <hungnqse150125@fpt.edu.vn>
2023-06-29 22:16:55 +0700

    add authen and deploy

---
+++ API/Controllers/Authentication.cs
@@ -0,0 +1,89 @@
+﻿using DataLayer.Models;
+using DataLayer.UnitOfWork;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace API.Controllers
+{
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IUnitOfWork _unitOfWork;   
+        public AuthenticationController(IConfiguration configuration, IUnitOfWork unitOfWork)
+        {
+            _configuration = configuration;
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginInfo loginInfo)
+        {
+            var account = new Account
+            {
+                AccountId = 1,
+                UserName = loginInfo.UserName,
+            };
+            String role;
+            if (loginInfo.UserName.Contains("a"))
+            {
+                role = "USER";
+            }
+            else
+            {
+                role = "ADMIN";
+            }
+            // Create claims for the authenticated user
+            var claims = new[]
+            {
+            new Claim(ClaimTypes.NameIdentifier, account.AccountId.ToString()),
+            new Claim(ClaimTypes.Name, account.UserName),
+            new Claim(ClaimTypes.Role, role)
+            // Add additional claims as needed
+        };
+
+            // Generate JWT token
+            var token = GenerateJwtToken(claims);
+
+            // Return the token to the client
+            return Ok(new { Token = token });
+        }
+        [HttpGet("user")]
+        [Authorize(Roles ="USER")]
+        public IActionResult GetUser()
+        {
+            return Ok("OK bitch User");
+        }
+        [HttpGet("admin")]
+        [Authorize(Roles = "ADMIN")]
+        public IActionResult GetAdmin()
+        {
+            return Ok("OK bitch Admin");
+        }
+        private string GenerateJwtToken(Claim[] claims)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Authentication:SecretKey"]));
+
+            var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_configuration["Authentication:Issuer"],
+                                            _configuration["Authentication:Audience"],
+                                            claims, expires: DateTime.UtcNow.AddDays(1),
+                                            signingCredentials: signIn);
+            var writedToken = new JwtSecurityTokenHandler().WriteToken(token);
+            return writedToken;
+        }
+    }
+   
+    public class LoginInfo
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -6,7 +6,11 @@ using BusinessLayer.Services.AccountService;
 using DataLayer.Models;
 using DataLayer.Repositories;
 using DataLayer.UnitOfWork;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -38,7 +42,51 @@ services.AddScoped<IProductCampaignService, ProductCampaignService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+               .AddJwtBearer(options =>
+               {
+                   options.TokenValidationParameters = new TokenValidationParameters
+                   {
+                       ValidateIssuer = true,
+                       ValidateAudience = true,
+                       ValidateLifetime = true,
+                       ValidateIssuerSigningKey = true,
+                       ValidIssuer = Configuration["Authentication:Issuer"],
+                       ValidAudience = Configuration["Authentication:Audience"],
+                       IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Authentication:SecretKey"]))
+                   };
+               });
+services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
+
+    // Add JWT token support
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme.",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    },
+                    Scheme = "oauth2",
+                        Name = "Bearer",
+                        In = ParameterLocation.Header,
+                },
+                new string[] { }
+            }
+        });
+});
 
 var app = builder.Build();
 
@@ -52,6 +100,7 @@ if (app.Environment.IsDevelopment())
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
